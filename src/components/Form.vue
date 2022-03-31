8844553b75e0681cdd63cbb4856dbc8872b147a2
Aleksandr <interind@yandex.ru>
2022-03-31 19:43:32 +0500

    style-list

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -13,9 +13,22 @@ export default {
       name: '',
       age: '',
       children: [],
-    }
+    },
+    childProfile: {
+      id: 0,
+      name: '',
+      age: '',
+    },
   }),
   methods: {
+    addChild() {
+      this.profile.children.push(this.childProfile);
+      this.childProfile = { id: this.profile.children.length, name: '', age: ''};
+      console.log(this.profile.children);
+    },
+    deleteItem(idKey) {
+      this.profile.children = this.profile.children.filter(({id}) => id !== idKey);
+    },
     checkInput: function () {
       this.errors = {name: '', age: ''};
       if (!this.profile['name']) {
@@ -43,17 +56,23 @@ export default {
     },
   },
   computed: {
+    statusList() {
+      return this.profile.children.length;
+    },
     getStatusButton() {
       console.log(Object.values(this.errors));
       return Object.values(this.errors).filter((el) => el).length;
     },
+    statusShowAddButton() {
+      return this.profile.children.length < 5;
+    }
   }
 };
 
 </script>
 
 <template>
-  <form class="form">
+  <form class="form" @submit.prevent="print(profile)">
     <h1 class="form__title">
       Персональные данные
     </h1>
@@ -73,7 +92,7 @@ export default {
       id="age"
       nameInput="age"
       type="number"
-      min=1
+      min=15
       max=99
       classForm="form"
       :validator="checkInput"
@@ -86,10 +105,55 @@ export default {
       <h2 class="form__title">
         Дети (макс.5)
       </h2>
-      <button class="button button__add" @click="print(profile)" type="button" :disabled="getStatusButton">
+      <button
+        v-show="statusShowAddButton"
+        class="button button__add"
+        @click="addChild"
+        type="button"
+      >
+        <div />
         Добавить ребенка
       </button>
     </div>
+    <transition-group v-show="statusList" name="list-complete" class="form__list" tag="ul">
+      <li v-for="child in profile.children" :key="child.id" class="list-complete-item form__list-item">
+        <InputMy
+          id="name"
+          nameInput="name"
+          type="text"
+          minlength=2
+          classForm="form"
+          :validator="checkInput"
+          :msgError="errors.name"
+          msgPlaceholder="Имя"
+          :valueCheck="childProfile.name"
+          v-model.capitalize="childProfile.name"
+       />
+      <InputMy
+        id="age"
+        nameInput="age"
+        type="number"
+        min=1
+        max=80
+        classForm="form"
+        :validator="checkInput"
+        :msgError="errors.age"
+        msgPlaceholder="Возраст"
+        v-model="childProfile.age"
+        :valueCheck="childProfile.age"
+      />
+      <button
+        class="button button__delete"
+        @click="deleteItem(child.id)"
+        type="button"
+      >
+        Удалить
+      </button>
+      </li>
+    </transition-group>
+    <button v-show="statusList" class="button button__submit" type="submit" :disabled="getStatusButton">
+      Сохранить
+    </button>
   </form>
 </template>
 
